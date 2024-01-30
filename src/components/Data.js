1333a8c6495d99ad1084b741541f47922a19922e
naufilswebsite <naufilshaikh2002@gmail.com>
2024-01-30 18:25:10 +0530

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,28 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg",
+    title: "Don't eat less. Just eat real.",
+  },
+  {
+    id: 2,
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/01/18/46/cook-2364221_640.jpg",
+    title: "Don't eat less. Just eat real.",
+  },
+  {
+    id: 3,
+    image:
+      "https://thumbs.dreamstime.com/b/italian-food-background-variety-ingredients-over-slate-35112782.jpg",
+    title: "Don't eat less. Just eat real.",
+  },
+  {
+    id: 4,
+    image:
+      "https://us.123rf.com/450wm/tommisch/tommisch1804/tommisch180400573/100151992-pasta-and-ingredients-on-wooden-background-with-copy-space-top-view-vegetarian-food-healthy-or.jpg?ver=6",
+    title: "Don't eat less. Just eat real.",
+  },
+];
+
+export default people;
